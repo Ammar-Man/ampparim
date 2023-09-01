5f2acf4314bdbc2a327c39b61aa9b4bec1a73fef
Ammar-Man <almandaa@arcada.fi>
2023-09-02 02:47:22 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import './assets/main.css'
+
+
+
+import {createApp } from "vue"
+
+
+import App from "./App.vue"
+import BikeItem from '../components/BikeItem.vue'
+
+const app = createApp(App)
+createApp(App).mount("#app")
+
